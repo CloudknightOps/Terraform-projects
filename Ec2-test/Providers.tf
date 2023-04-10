94f830b9ac5f76d53fc934d4d9da35f824e00ce9
Theoooo <Uviekugbere.theo@gmail.com>
2023-04-10 14:54:01 +0100

    Restructured files

--- Ec2-test/Providers.tf
+++ Ec2-test/Providers.tf
@@ -2,14 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region                   = "us-east-1"
+  region     = var.aws_region
+
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "Vscode"
+  profile                  = "Theolab22"
 }
+
